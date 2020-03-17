9abe97716c7b893f4a85e120a359a67fa50e0fd5
D.Mansurov <d.mansurov@krastsvetmet.ru>
2020-03-17 17:23:54 +0700

    Немного сидов богу сидов

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -17,4 +17,8 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     protected $table = 'roles';
+
+    public const ROLE_ADMIN = 1;
+    public const ROLE_MANAGER = 2;
+    public const ROLE_USER = 3;
 }
